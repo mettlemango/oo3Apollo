b19a9f0a065ad9c4fc62bd1d21e9d73c8d54778c
carllsimonlin <carll_lin@dlsu.edu.ph>
2023-07-21 21:12:21 +0800

    Code for Final Project

---
+++ Mike's Beach Adventure/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+                                requires com.almasb.fxgl.all;
+    
+    opens com.example.demo to javafx.fxml;
+    exports com.example.demo;
+}
